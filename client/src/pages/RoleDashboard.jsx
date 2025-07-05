da459313ab115391eb4769b7fceefeaf85fa3078
Deo-Siatah <deosiatah0@gmail.com>
2025-07-05 17:59:36 +0300

    adding the frontend part to backend

---
+++ client/src/pages/RoleDashboard.jsx
@@ -0,0 +1,23 @@
+import { getUserRole } from "../utils/auth";
+import AdminDashboard from "./AdminDashboard";
+import DeveloperDashboard from "./Dashboard";
+import { Navigate } from "react-router-dom";
+
+export default function RoleDashboard() {
+    const userRole = getUserRole();
+
+    //if no role is found ,redirect to login
+    if (!userRole) {
+        return <Navigate to = "/login" replace/>
+    }
+
+    //Render appropriate dashoard based on role
+    switch (userRole){
+        case 'admin':
+            return <AdminDashboard/>;
+        case 'developer':
+            return <DeveloperDashboard/>;
+        default:
+            return <Navigate to = "/login" replace/>;
+    }
+}
